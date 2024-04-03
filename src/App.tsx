54b10dc4ad04125e2cb999383319284b75bd38b7
Patrick Thompson <patrick_thompson9@icloud.com>
2024-04-03 17:10:01 -0300

    added react import and removed test header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import ListGroup from "./Components/ListGroup";
+import React from "react";
 
 function App() {
   let items = ["New York", "San fran", "London", "Paris"];
   return (
     <div>
       <ListGroup items={items} heading="Cities" />
-      <h1>TEST</h1>
     </div>
   );
 }
