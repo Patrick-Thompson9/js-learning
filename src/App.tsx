0d41098e5cff4a9df76eb098667195fbf67bcaf7
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-04 21:08:08 -0300

    Made the button make the alert visible and the alert closable

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Alert from "./Components/Alert";
 import Button from "./Components/Button";
+import { useState } from 'react';
 
 function App() {
-  const handleEvent = (num: number) => {
-    console.log(`CLICKED YO! ${num}`)
+
+  const [alertVisibility, setAlertVisibility] = useState(false);
+
+  const handleAddAlert = () => {
+    console.log('Button clicked!');
+    setAlertVisibility(true);
   }
 
   return (
     <div>
-      <Alert>
-        Hello from the <strong>alert!</strong>
-      </Alert>
-      <Button children="Click Me!" onClick={handleEvent} color="secondary"/>
+      {alertVisibility && <Alert onClose={() => setAlertVisibility(false)}><strong>Alert!</strong> You clicked the button!</Alert>}
+      <Button children="Click Me!" onClick={handleAddAlert} color="secondary"/>
     </div>
   );
 }
--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,14 +2,14 @@ import {ReactNode} from 'react';
 
 interface ButtonProps {
     children: ReactNode;
-    onClick: (num: number) => void;
+    onClick: () => void;
     color?: "primary" | "secondary" | "success" | "danger" | "white" | "muted" | "primary-outline" | "secondary-outline";
 }
 
 const Button = ({ children, onClick, color='primary'}: ButtonProps) => {
 
     return (
-        <button className={'btn btn-'+ color} onClick={() => onClick(7)}>{children}</button>
+        <button className={'btn btn-'+ color} onClick={() => onClick()}>{children}</button>
     )
 }
 
